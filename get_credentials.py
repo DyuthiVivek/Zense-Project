f3e7f19168b671902a874ebdb1fac0db472cba6a
DyuthiVivek <116893588+DyuthiVivek@users.noreply.github.com>
2023-08-27 17:01:36 +0530

    Update get_credentials.py

--- get_credentials.py
+++ get_credentials.py
@@ -4,7 +4,7 @@ file = 'config.encrypted' # filename
 conf_file = ConfigParserCrypt()
 
 # Set AES key
-conf_file.aes_key = b'\xa1^\x03\xaa\xeb\xcc\x1aX\xd6\x1fAR.8\xfa\x9b\xfa3$\xf7\xad\x18\x05\xb2\xcf_\xa3\x03\xe3)\xd9H'
+conf_file.aes_key = 'Paste your AES key here'
 # Read from encrypted config file
 conf_file.read_encrypted(file)
 
